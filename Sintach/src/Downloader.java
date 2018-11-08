06b8d2edcbb712eda8d76cf59e0f6bef60ee4318
OE2MAL Markus <markus.liebminger@sbg.at>
2018-11-08 10:47:45 +0100

    First step into a new world...

---
+++ Sintach/src/Downloader.java
@@ -0,0 +1,46 @@
+
+
+import java.net.*;
+import java.io.*;
+// import java.util.*;
+
+public class Downloader {
+
+	public void loader(String urlLink, String file) {
+				
+		String url = urlLink;
+		String filePath = file;	
+		
+		try { 
+			
+			URL urlObj = new URL(url);
+			URLConnection urlCon = urlObj.openConnection();
+			
+			InputStream inputStream = urlCon.getInputStream();
+			BufferedInputStream reader = new BufferedInputStream(inputStream);
+			
+			BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(filePath));
+			
+			byte[] buffer = new byte[4096];
+			int bytesRead = -1;
+			
+			while((bytesRead = reader.read(buffer)) != -1) {
+				writer.write(buffer, 0, bytesRead);
+			}
+			
+			writer.close();
+			reader.close();
+															
+		} catch (MalformedURLException e){
+			System.out.println("The specified URL is malformed: " + e.getMessage());
+			
+		} catch (IOException e){
+			System.out.println("An I/O error occurs: " + e.getMessage());
+			
+		}
+		
+		return;
+			
+	}
+
+}
---
+++ Sintach/src/Runner.java
@@ -0,0 +1,24 @@
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		String ICAO = "LOWS";
+		
+		String file = ICAO + ".txt";
+		
+		String link = "ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/";
+		
+		String urlLink = link + ICAO + ".TXT";
+		
+		Downloader dl = new Downloader();
+		TextFileReader tfr = new TextFileReader();
+		
+		dl.loader(urlLink, file);
+		tfr.textreader(file);
+					
+
+	}
+
+}
