062b3543b0b69fa0ed5596f0f7bf02209c32b248
OE2MAL Markus <markus.liebminger@sbg.at>
2018-11-17 13:50:21 +0100

    Userinput implementation

--- Sintach/src/edu/sintach/util/Main.java
+++ Sintach/src/edu/sintach/util/Main.java
@@ -1,16 +1,32 @@
 package edu.sintach.util;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		String icao = "LOWS";
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println();
+		String icao = "";
+		
+		while(icao.isEmpty() || icao.length() < 4 || icao.length() >= 5){
+			System.out.println("Please enter ICAO Code: ");
+		
+			icao = keyboard.next().toUpperCase();
+		}
+		
+		keyboard.close();
+				
 		String localPath = "/home/mlieb/metar_loader/";
 		String localFile = icao + ".txt";
 		String link = "ftp://tgftp.nws.noaa.gov/data/observations/metar/stations/";
-		
-		
+					
 		new Runner(icao, localPath, localFile, link);
+		
+		System.out.println();
+		System.out.println("Ready!");
 	}
 
 }
